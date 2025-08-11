940a94d4a4c59f61e73323027eb0c95836ec2908
lamvinhkien <lamvinhkien1709@gmail.com>
2025-08-11 16:47:43 +0700

    test audio mobile

--- src/Gecko.js
+++ src/Gecko.js
@@ -36,6 +36,7 @@ const Gecko = () => {
   };
 
   useEffect(() => {
+    // Cập nhật chiều cao viewport
     const setVh = () => {
       document.documentElement.style.setProperty(
         "--vh",
@@ -46,6 +47,18 @@ const Gecko = () => {
     window.addEventListener("resize", setVh);
     return () => window.removeEventListener("resize", setVh);
   }, []);
+
+  useEffect(() => {
+    // Tự động phát khi vào trang
+    if (audioRef.current) {
+      audioRef.current.volume = value / 100;
+      audioRef.current.play()
+        .then(() => setIsPlaying(true))
+        .catch(err => {
+          console.log("Autoplay bị chặn:", err);
+        });
+    }
+  }, []); // Chỉ chạy khi load lần đầu
 
   return (
     <div className="setting-container">
